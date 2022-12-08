eb88b428f1d7d90136effa466bbfa1275ccb571f
TechOgede <sammonius@live.com>
2022-12-08 14:04:46 -0800

    printing sizes in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,13 @@ int main(void)
 	int i;
 	char c;
 	float f;
+	long int x;
+	long long int y;
 
-	printf("The size of an integer is: %lu\n", sizeof(i));
-	printf("The size of a character is: %lu\n", sizeof(c));
-	printf("The size of a float is: %lu\n", sizeof(f));
+	printf("The size of an integer is: %lu byte(s)\n", sizeof(i));
+	printf("The size of a character is: %lu byte(s)\n", sizeof(c));
+	printf("The size of a float is: %lu\ byte(s)\n", sizeof(f));
+	printf("The size of a long integer is: %lu byte(s)\n", sizeof(x));
+	printf("The size of a long long integer is: %lu byte(s)\n", sizeof(y));
 	return (0);
 }
