30b839a7100e8ae026c549a567baceb41745bb03
TechOgede <sammonius@live.com>
2022-12-08 13:23:57 -0800

    print to stdout using printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - Main vehicle for programs in C
+ *
+ * Description: C requires all programs be run in function
+ * Return: 0 if successful, error if not.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
