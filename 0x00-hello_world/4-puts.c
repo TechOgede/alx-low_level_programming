aeb6555df4b4868c8ee162acf1a8c64faa01ee2d
TechOgede <sammonius@live.com>
2022-12-08 12:56:21 -0800

    printing to stdout using put()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
-
+/**
+ * main - provides a vehicle for the entire program
+ *
+ * Description: C being a functional imperative langauge, requires a main function be used to run the entire program.
+ * Return: 0 if successful, error if not.
+ */
 int main(void)
 {
 	char *stmt = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(stmt);
 
-	return 0;
+	return (0);
 }
