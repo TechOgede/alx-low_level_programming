b7eb7b0d4640e44463195c93dabcafd14115d2f0
TechOgede <sammonius@live.com>
2022-12-08 13:42:14 -0800

    print the size in bytes of the various types in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - vehicle for all programs in C
+ *
+ * Description: C requires that all programs be
+ * run in a main function.
+ * Return: 0 if successful, error if not
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+	float f;
+
+	printf("The size of an integer is: %lu\n", (unsigned long)(sizeof(i));
+	printf("The size of a character is: %lu\n", (unsigned long)(sizeof(c));
+	printf("The size of a float is: $lu\n", (unsigned long)(sizeof(f));
+	
+	return (0);
+
+}
