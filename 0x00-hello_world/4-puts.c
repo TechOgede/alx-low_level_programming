10c1dfb949040eda263943f8d470fb9369fff72e
TechOgede <sammonius@live.com>
2022-12-08 11:10:06 -0800

    printing to stdout using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char *stmt = '"Programming is like building a multilingual puzzle\n';
+	char *stmt = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(stmt);
 
