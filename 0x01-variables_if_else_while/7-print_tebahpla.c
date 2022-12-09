a2d59bb71c0250b7c29624f13cf91570e37e2fe0
TechOgede <sammonius@live.com>
2022-12-09 08:25:34 -0800

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: (0) if successful
+ */
+
+int main(void)
+{
+	char rev;
+
+	for (rev = 'z'; rev >= 'a'; rev--)
+	{
+		putchar(rev);
+	}
+	putchar('\n');
+	return (0);
+}
+
