b8d9475dcbf7e6be4ac626eaf7830f08403da034
TechOgede <sammonius@live.com>
2022-12-08 17:10:02 -0800

    print using write()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 /*
  * main - Entry point
  *
+ * Description: required by C
  * Return: Always (1) successful
  */
 
 int main(void)
 {
-	char *str = "and that piece of art is useful\", - Dora Korpar, 2015-10-19";
+	char *str = "and that piece of art is useful\", - Dora Korpar, 2015-10-19\n";
 
 	write(2, str, 59);
 	return (1);
