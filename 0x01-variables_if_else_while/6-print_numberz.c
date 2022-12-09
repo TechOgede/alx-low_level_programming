6743d0af2820eeab305d1de0c9ef0ff296720d4c
TechOgede <sammonius@live.com>
2022-12-09 08:12:07 -0800

    counting from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+i#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: (0) if successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
+
