4eae147bb29ce342ed1df7135245a8995de788b5
TechOgede <sammonius@live.com>
2022-12-09 23:47:33 -0800

    print sequence of two numbers of two digits each

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,31 +8,23 @@
 
 int main(void)
 {
-	int i;
-	int j;
-	int k;
+	int i, j;
 
-	for (i = '0'; i < '9'; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = i + 1; j <= '9'; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (k = j + 1; k <= '9'; k++)
-			{
-				if ((j != i) != k)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-
-					if (i == '7' && j == '8')
-						continue;
-
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && q == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
+
 	}
 	putchar('\n');
 	return (0);
