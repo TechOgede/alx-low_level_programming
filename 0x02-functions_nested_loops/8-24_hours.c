825e8025ba3dae391e0cab89b997e14690600b6d
TechOgede <sammonius@live.com>
2022-12-13 15:20:24 -0800

    every minute of a 24 hour period

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * 24_hours - prints every minute in a 24 hour period
+ * jack_bauer - prints every minute in a 24 hour period
  *
  * Description: Using nested loops to print time as a four digit figure
  * with a middle colon.
@@ -10,6 +10,17 @@
 
 void jack_bauer(void)
 {
-	for (i = 0; i < 3; i++)
-	{	for(j = 0; j
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{	_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
 	}
+}
