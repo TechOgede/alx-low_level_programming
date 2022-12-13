f777632f941f1e83d5380692dcc8a6d73207f46e
TechOgede <sammonius@live.com>
2022-12-13 14:50:52 -0800

    print even fib numbers < 4M

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	long int i, x, y, next, flag, sum_even;
+
+	x = 0;
+	y = 1;
+	next = x + y;
+	flag = 1;
+	sum_even = 0;
+
+	while (flag)
+	{
+		if (next <= 4000000)
+		{
+			x = y;
+			y = next;
+			next = x + y;
+
+			if (next % 2 == 0)
+			{
+				sum_even += next;
+			}
+		}
+		else
+			flag = 0;
+	}
+	printf("%ld", sum_even);
+	printf("\n");
+	return (0);
+}
