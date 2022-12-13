8afb3c62ad9294b98b4f8e7b55b4e568ac950101
TechOgede <sammonius@live.com>
2022-12-13 12:45:40 -0800

    count up or down to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
  * print_to_98 - print natural numbers from n to 98
  * @n: an integer
  *
- * Description: using a loop to count upwards
+ * Description: using a loop to count
  * Return: no return value
  */
 
@@ -12,8 +12,27 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
-	{	printf("%d, ", i);
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{	
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d", i);
+		}
+	
 	}
+
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d", i);
+		}
+	{
 	printf("\n");
 }
