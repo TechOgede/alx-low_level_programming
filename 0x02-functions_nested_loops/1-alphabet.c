cdeec1d2f03acfa236c83a77c1f1fbda9c837099
TechOgede <sammonius@live.com>
2022-12-13 06:15:20 -0800

    print the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+void print_alphabet(void);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * print_alphabet - print the letter in lowercase
+ *
+ * Description: uses putchar to print the alphabet in lowercase
+ * Return: Has no return value
+ */
+
+void print_alphabet(void)
+{	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		putchar(ch);
+	}
+}
