72976f1577348f016e534b2dc85eab3460b401f7
TechOgede <sammonius@live.com>
2022-12-13 09:31:40 -0800

    determine sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,20 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar(43);
+		putchar(49);
 		return (1);
 	}
 
 	else if (n < 0)
 	{
 		putchar(45);
+		putchar(40);
 		return (-1);
 	}
 
 	else
 	{
+		putchar(48);
 		putchar(48);
 		return (0);
 	}
