4a77700bd6905b25998541c69ff7443c40e58da5
TechOgede <sammonius@live.com>
2022-12-13 11:38:34 -0800

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - sum two integers
+ * @i: first integer
+ * @j: second integer
+ *
+ * Description: simple addition
+ * Return: an integer
+ */
+
+int add(int i, int j)
+{	sum = i + j;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * 24_hours - prints every minute in a 24 hour period
+ *
+ * Description: Using nested loops to print time as a four digit figure
+ * with a middle colon.
+ * Return: doesnt return any value
+ */
+
+void jack_bauer(void)
+{
+	for (i = 0; i < 3; i++)
+	{	for(j = 0; j
+	}
