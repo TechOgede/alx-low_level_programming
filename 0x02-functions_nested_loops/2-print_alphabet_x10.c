cd4bd978361c3b31ea056f931c77fd7ea726757d
TechOgede <sammonius@live.com>
2022-12-13 07:23:34 -0800

    print lowercase alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,12 +13,11 @@ void print_alphabet_x10(void)
 	int ch;
 
 	for (i = 0; i < 10; i++)
-	{	
+	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			putchar(ch);
-			putchar('\n');
 		}
+		putchar('\n');
 	}
 }
-		
