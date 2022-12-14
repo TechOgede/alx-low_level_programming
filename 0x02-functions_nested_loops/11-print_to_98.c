90199b7e6d735d122eecebff16b8318bb430b6f5
TechOgede <sammonius@live.com>
2022-12-14 00:28:59 -0800

    print up or down to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,47 +8,27 @@
  * Return: no return value
  */
 
-void print_to_98(int n) {
-
-	int i;
-
-	if (n <= 98)
+void print_to_98(int n)
+{
+	if (n > 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (;n > 98; n--)
 		{
-			if (i != 98)
-				printf("%d, ", i);
-			else
-				printf("%d", i);
+			printf("%d, ", n);
+		
+		}
+	
+	}
+
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+
 		}
 
 	}
-
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-			else
-				printf("%d", i);
-		}
-	{
-
-	printf("\n");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	printf("%d\n", n);
 
 }
---
+++ 0x02-functions_nested_loops/500.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers from n to 98
+ * @n: an integer
+ *
+ * Description: using a loop to count
+ * Return: no return value
+ */
+
+void print_to_98(int n) {
+
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d", i);
+		}
+
+	}
+
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d", i);
+		}
+	{
+
+	printf("\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
