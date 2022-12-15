698f93ebd512d8b8d47f6b7dab993a3a566f3290
TechOgede <sammonius@live.com>
2022-12-15 11:25:00 -0800

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,13 +12,16 @@ void print_line(int n)
 {
 	int i:
 
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		if (n <= 0)
-			break;
-		_putchar('_');
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
 	}
-	_putchar('\n');
+
 }
 
 
