2e0254e80dd051b593fac6313da04b486a91693d
TechOgede <sammonius@live.com>
2022-12-15 09:55:38 -0800

    display nos 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_numbers - displays nums 0 to 9
+*
+* Description: uses a for loop to display the numbers
+* Return: no return value
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
+
