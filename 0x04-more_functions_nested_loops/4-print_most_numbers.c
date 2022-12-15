5af40506aac9790acf7a56de5e8bf47358e1cd96
TechOgede <sammonius@live.com>
2022-12-15 10:01:56 -0800

    display nos 0 to 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_most_numbers - display numbers btw 0 and 9 except 2 and 4
+*
+* Description: uses a for loop to display numbers
+* Return: no return value
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if ( i != 50 || i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
