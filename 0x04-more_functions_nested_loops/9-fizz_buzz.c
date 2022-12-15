51e3a52a0fcbc30acbd6c06471c219fa5285f9b0
TechOgede <sammonius@live.com>
2022-12-15 13:15:10 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: no return value
+*/
+
+int main(void)
+{
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+
+		else if (i % 5 == 0)
+			printf("Buzz ");
+
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+
+	}
+	printf("\n");
+	return (0);
+
+}
