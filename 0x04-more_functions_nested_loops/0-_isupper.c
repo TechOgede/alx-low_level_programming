87e259ebf940eb54a9f7b73127bc7a28e240689f
TechOgede <sammonius@live.com>
2022-12-15 08:17:54 -0800

    determine if char is uppercase

---
+++ 0x04-more_functions_nested_loops/0-_isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isupper - determines if a character is uppercase or not
+* @c: an ASCII character
+*
+* Return: 1 if uppercase, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+#include<stdio.h>
+int _putchar(int ch);
+int _isupper(int c);
