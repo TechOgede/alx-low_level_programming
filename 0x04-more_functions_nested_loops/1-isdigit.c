046009579e09e1ae40cc33b1806741109ce74233
TechOgede <sammonius@live.com>
2022-12-15 08:55:29 -0800

    determine if char is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+##include "main.h"
+
+/**
+* _isdigit - determines if a character is a digit from 0 to 9
+* @c: a number in single quotes
+*
+* Description: uses the ASCII chart as a standard
+* Return: 1 if a digit, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	
+	else
+		return (0);
+}
+
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,6 @@
 
 int _putchar(int ch);
 int _isupper(int c);
-
+int _isdigit(int c);
 
 #endif /* MAIN_H */
