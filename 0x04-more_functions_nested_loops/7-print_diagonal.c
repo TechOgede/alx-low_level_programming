0b3fa736462af4e618c8c2a426f1c9d8d1c53115
TechOgede <sammonius@live.com>
2022-12-15 12:52:46 -0800

    print \ diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* print_diagonal - prints \ multiple times diagonally
+* @n: any positive integer
+*
+* Description: using a for loop to print \ n times
+* Return: no return value
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(32);
+
+			_putchar(92);
+			_putchar(10);
+		}
+
+	}
+
+	else
+		_putchar(10);
+
+}
+
+
+
