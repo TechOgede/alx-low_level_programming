350b2806ce736d2f136517a3a651eaa6f62dcf0c
TechOgede <sammonius@live.com>
2022-12-19 14:32:47 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<string.h>
+
+/**
+* rev_string - reverses a string
+* @s: a null termianted string
+*
+* Description: reverses a string in state
+* stops when the Null character is detected.
+* Return: no return value
+*/
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	char tmp;
+	int i;
+
+	while (i < len--)
+	{
+		tmp = s[i];
+		s[i++] = s[len];
+		s[len] = tmp;
+	}
+}
