f67c5205d4b37af7df266ea75b6199608002c57d
TechOgede <sammonius@live.com>
2022-12-21 04:26:45 -0800

    concatenate two strings using # of bytes as a requirement

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings
+* @dest: destination string
+* @src: string to be concatenated onto dest
+* @n: a positive integer denoting the max number of bytes (or chars)
+* of src to be concatenated to dest.
+* Description: Similar to strcat except that src doesnt have to be null
+* terminated if it contains n  or more bytes.
+* Return: returns a pointer to the first character of the new dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, dest_len = 0, src_len = 0;
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	for (i = 0; i < n; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + (i + 1)] = '\0';
+	return (dest);
+}
