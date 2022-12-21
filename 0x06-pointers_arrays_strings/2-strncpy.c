32d1f71988ea523a3c51bb2535173c57fc161955
TechOgede <sammonius@live.com>
2022-12-21 05:29:33 -0800

    copy n bytes of a string to a buffer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncpy - copies n bytes of a string to a destination buffer
+*@dest: destination buffer
+*@src: string to be copied
+*@n: number of bytes of src to be copied
+*
+* Description: loops through src and copies each element to dest
+* Return: returns a string (char *)
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i <= n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
