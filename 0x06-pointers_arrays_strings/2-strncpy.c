de6ceb3722ae4039385a6340bbd8e99acce7381f
TechOgede <sammonius@live.com>
2022-12-22 02:16:42 -0800

    copy n bytes of a string to a dest buffer

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,15 +16,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[src_len] != '\0')
 		src_len++;
-	if (n > src_len)
-		n = src_len;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-
-	for (; i <= n; i++)
+	for (; i < n; i++)
 		dest[i] = '\0';
+
 	return (dest);
 }
