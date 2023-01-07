5ba3a3d97343dc8a43dd6ae63df742a098e02d14
TechOgede <sammonius@live.com>
2023-01-07 03:24:49 -0800

    add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<limits.h>
 
+int _atoi(char *str);
 /**
 * main - Entry point
 * @argc: no of arguments
@@ -18,16 +20,43 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0)
+		if (_atoi(argv[i]) == INT_MIN)
 		{	printf("Error\n");
 			return (1);
 		}
 
-		sum += atoi(argv[i]);
+		sum += _atoi(argv[i]);
 	
 
 	}
 	if (argc != 1)
 		printf("%d\n", sum);
 	return (0);
+}
+
+/**
+* _atoi - converts a string to an integer
+* @str: string to be converted
+*
+* Description: converts strings of numbers to int. Even accepts the 
+* prceding + or -
+* Return: returns the integer if successful. 0 otherwise
+*/
+
+int _atoi(char *str)
+{
+	int res = 0, i, sign = 1;
+
+	if (str[0] == '-')
+		sign = -1;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (str[i] >= 48 && str[i] <= 57)
+		res = res * 10 + str[i] - '0';
+		else
+			return (INT_MIN);
+	}
+	return (sign * res);
+
 }
