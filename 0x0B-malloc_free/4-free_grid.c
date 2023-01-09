f061c2c2b7e50263edec254365d2245895a193d5
TechOgede <sammonius@live.com>
2023-01-09 11:01:28 -0800

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* free_grid - frees the memory allocated to a grid of integers
+* @grid: a pointer to a pointer
+* @height: int specifying the height of the grid
+*
+* Description: frees dynamically allocated memory used to create the grid
+* Return: no return value
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	
+	free(grid);
+
+}
