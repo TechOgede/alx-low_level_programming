438bfbe56d64e086c2ae61bba932468c3a6f1b1e
TechOgede <sammonius@live.com>
2023-01-13 07:30:55 -0800

    reallocate memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -43,10 +43,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		for (i = 0; i < new_size; i++)
 			new_mem[i] = new_ptr[i];
 
+		free(new_ptr);
 		return (new_mem);
 	}
 
 	for (i = 0; i < old_size; i++)
 		new_mem[i] = new_ptr[i];
+	free(new_ptr);
 	return (new_mem);
 }
