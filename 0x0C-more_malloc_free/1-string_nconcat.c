6b4aa01c0c898f41e80937b40a393fb7d739caca
TechOgede <sammonius@live.com>
2023-01-13 01:09:35 -0800

    concat strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, k = 0, len_s1 = 0, len_s2 = 0;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		len_s1++;
