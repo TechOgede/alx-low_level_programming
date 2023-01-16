a81547031704a24d7aea74359ba16858b6fd62fb
TechOgede <sammonius@live.com>
2023-01-16 13:12:20 -0800

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - Dog data
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner's name
+*
+* Description: contains 3 data fields
+*/
+
 struct dog
 {
 	char *name;
