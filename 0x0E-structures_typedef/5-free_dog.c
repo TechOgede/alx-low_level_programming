20e66b4f69499d8d5dfea83a61c57d8c03158c72
TechOgede <sammonius@live.com>
2023-01-17 02:40:22 -0800

    free blocks allocating during dog creation

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+* free_dog - frees data blocks allocated in creating dog_t
+* @d: pointer to the struct dog_t
+*
+* Description: uses free() to free prev allocated memory
+* Return: no return value
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
