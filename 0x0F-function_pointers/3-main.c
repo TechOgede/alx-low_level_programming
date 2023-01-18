db3d832eb8b2b338e84734e1367ab96d6bc01fc5
TechOgede <sammonius@live.com>
2023-01-18 14:54:11 -0800

    peform simple arithmeti

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,29 +9,31 @@
 
 int main(int argc, char **argv)
 {
-	int a, b, error = 0;
+	int a, b;
 	int (*op_ptr)(int, int);
-	int opr = *argv[2];
+	char *opr = argv[2];
 
 	if (argc != 4)
 	{	printf("Error\n");
 		exit(98);
 	}
-	if (opr != 43 && opr != 45 && opr != 42 && opr != 47 && opr != 37)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	if ((opr == '/' || opr == '%') && b == 0)
+	if ((*opr == '/' || *opr == '%') && b == 0)
 	{	printf("Error\n");
 		exit(100);
 	}
 
-	op_ptr = get_op_func(*(argv + 2));
-	(op_ptr != NULL) ? printf("%d\n", op_ptr(a, b)): error++;
+	op_ptr = get_op_func(argv[2]);
+	if (op_ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+
+	}
+	printf("%d\n", op_ptr(a, b));
 
 	return (0);
 
