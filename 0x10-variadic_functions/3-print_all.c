c4de38a66dd6dc1a8dbcef7c0ed9661283b57396
TechOgede <sammonius@live.com>
2023-01-19 14:20:26 -0800

    prolly a printf v0.1

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+
+/**
+* print_all - prints everything
+* @format: char array of argument types
+*
+* Description: employs the use of va macros
+* Return: no return value
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int i = 0;
+	char *str_arg;
+
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+					break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+					break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+					break;
+			case 's':
+				str_arg = va_arg(ap, char *);
+				if (str_arg != NULL)
+				{	printf("%s", str_arg);
+					break;
+				}
+				printf("(nil)");
+				break;
+		}
+
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+				format[i] == 's') && format[i + 1] != '\0')
+			printf(", ");
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+
+}
