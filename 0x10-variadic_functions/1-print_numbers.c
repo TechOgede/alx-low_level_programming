a532127bfb5c0ededccdc0ec73376b2d2d68706b
TechOgede <sammonius@live.com>
2023-01-19 10:23:34 -0800

    print varying number of ints

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,12 +16,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 	for (i = 0; i < n - 1; i++)
-	{	if (separator != NULL)
+	{
+		if (separator != NULL)
 			printf("%d%s", va_arg(ap, int), separator);
 		else
 			printf("%d", va_arg(ap, int));
 	}
 	printf("%d", va_arg(ap, int));
 	printf("\n");
-
+	va_end(ap);
 }
