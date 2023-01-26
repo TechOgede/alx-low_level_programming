b413f79572469ebc321bba1bb93d94989686491e
TechOgede <sammonius@live.com>
2023-01-26 12:58:01 -0800

    free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_list - frees a linked list
+* @head: pointer to the start of list
+*
+* Description: frees each element of the list
+* Return: no return value
+*/
+
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		free(head);
+	}
+}
