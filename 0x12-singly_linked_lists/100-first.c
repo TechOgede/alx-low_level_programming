f795378e03b625cd2ade2d01c6adb34a863d0dbe
TechOgede <sammonius@live.com>
2023-01-26 14:49:45 -0800

    execute foo before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,5 @@
 
 __attribute__((constructor))void foo(void)
 {
-	printf("You're beat, yet you must allow,\nI bore my house upon my back\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
