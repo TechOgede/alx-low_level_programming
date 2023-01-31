15bbd9bdfd4f423f2fe497ea903a3e7e12b9b5b6
TechOgede <sammonius@live.com>
2023-01-31 01:18:52 -0800

    get a node in a list using  its index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && head; i++)
 	{
 		head = head->next;
 	}
