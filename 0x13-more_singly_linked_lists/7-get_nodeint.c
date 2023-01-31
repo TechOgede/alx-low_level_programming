a180a5db79ebf224cec62c0c124137ca583970d0
TechOgede <sammonius@live.com>
2023-01-31 01:16:47 -0800

    get a node in a list using  its index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - selects and returns a node in a list.
+* The node is specified by an index
+* @head: a pointer to the first node
+* @index: an int starting from 0
+*
+* Description: goes through the list up to the specified index,
+* detaches the needed node (by setting its next field to null)
+* Return: a pointer to the node at the index
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+	return (head);
+}
