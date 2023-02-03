d3815dcf7cc40ef5699d4517e8c49bcd0936827e
TechOgede <sammonius@live.com>
2023-02-03 12:25:19 -0800

    get the nth bit of a number

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,13 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 
+	if (index > 63)
+		return (-1);
+	if (n == 0)
+		return (0);
+
 	bit = (n >> index) & 1;
 
 	return (bit);
+
 }
