1cf47fcf1c0bccf3b2ac9389001a510e74ce496c
TechOgede <sammonius@live.com>
2023-02-06 13:47:24 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i]; i++)
 		len_text_content++;
 
-	fd = open(filename, O_CREAT | O_WRONLY| O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
