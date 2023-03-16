9a3b86f3e0f72acc0f1730fbb94b80fc02782271
TechOgede <sammonius@live.com>
2023-03-16 06:49:47 -0700

    insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,9 +42,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (pos == idx - 1)
 		{
 			new_node->next = head->next;
-			(head->next)->prev = new_node;
 			head->next = new_node;
 			new_node->prev = head;
+			if (new_node->next)
+				new_node->next->prev = new_node;
 		}
 		head = head->next;
 		pos++;
