b98c3930c40315e326db331e29f4d477b18380f7
TechOgede <sammonius@live.com>
2023-04-07 03:39:15 -0700

    [fix]:initialized all head pointers to NULL. hash_tables.h now betty compliant

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,10 +13,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t hash_table = {0, NULL};
 	hash_table_t *h_table;
 	hash_node_t **hash_node_array;
+	size_t i;
 
 	hash_node_array = malloc(sizeof(hash_node_t *) * size);
 	if (!hash_node_array)
 		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		hash_node_array[i] = NULL;
+	}
 	hash_table.size = size;
 	hash_table.array = hash_node_array;
 
