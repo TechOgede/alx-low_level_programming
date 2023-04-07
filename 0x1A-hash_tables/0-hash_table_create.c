2b19e0e1d3be3f5f17476c9836b7ce8bdc655c9f
TechOgede <sammonius@live.com>
2023-04-07 04:52:16 -0700

    allocated memory for the hash_table_t struct

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,23 +10,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t hash_table = {0, NULL};
-	hash_table_t *h_table;
+	hash_table_t *hash_table;
 	hash_node_t **hash_node_array;
 	size_t i;
 
+	hash_table = malloc(sizeof(hash_table_t));
+	if (!hash_table)
+		return (NULL);
 	hash_node_array = malloc(sizeof(hash_node_t *) * size);
 	if (!hash_node_array)
-		return (NULL);
-	for (i = 0; i < size; i++)
 	{
-		hash_node_array[i] = NULL;
+		free(hash_table);
+		return (NULL);
 	}
-	hash_table.size = size;
-	hash_table.array = hash_node_array;
+	for (i = 0; i < size; i++)
+		hash_node_array[i] = NULL;
+	hash_table->size = size;
+	hash_table->array = hash_node_array;
 
-	h_table = &hash_table;
-
-	return (h_table);
+	return (hash_table);
 }
 
