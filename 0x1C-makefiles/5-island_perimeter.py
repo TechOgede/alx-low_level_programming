70203a51589723e24442004e280ae49d43d33de1
TechOgede <sammonius@live.com>
2023-05-24 15:32:56 -0700

    determine island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -29,13 +29,13 @@ def island_perimeter(grid):
 
             if grid[i][j] == 1:
 
-                if (i - 1) == -1 or grid[i + 1][j] == 0:
+                if (i - 1) == -1 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if (i + 1) == num_rows or grid[i - 1][j] == 0:
+                if (i + 1) == num_rows or grid[i + 1][j] == 0:
                     perimeter += 1
-                if (j - 1) == -1 or grid[i][j + 1] == 0:
+                if (j - 1) == -1 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if (j + 1) == num_cols or grid[i][j - 1] == 0:
+                if (j + 1) == num_cols or grid[i][j + 1] == 0:
                     perimeter += 1
 
             j += 1
