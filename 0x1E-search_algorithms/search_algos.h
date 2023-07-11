f5e1cf38a6b020d3a6afc3efcafd258a89ebb708
TechOgede <sammonius@live.com>
2023-07-11 16:07:54 -0700

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,9 @@
 
 int linear_search(int *array, size_t size, int value);
 
+int binary_search(int *array, size_t size, int value);
+int binary_search_util(int *array, int low, int high, int value);
+void print_array(int *array, int low, int high);
 
 
 
