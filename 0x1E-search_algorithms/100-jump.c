3e4db8764c8792994a6e161a8e3aa37b6654cd03
TechOgede <sammonius@live.com>
2023-07-14 02:50:22 -0700

    jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+#include<math.h>
+
+/**
+* jump_search - Implementation of Jump Search Algo
+* @array: array of ints
+* @size: size of the array
+* @value: value to be searched for
+* Description: Jump search
+* Return: Index of value or -1 if
+* value is not present in array
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t m, i, k;
+
+	if (!array)
+		return (-1);
+
+	m = sqrt(size);
+
+	i = 0;
+
+	while ((i * m) < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i * m, array[i * m]);
+		if (array[i * m] > value)
+		{
+			if (i == 0)
+				k = 0;
+			else
+				k = (i - 1) * m;
+
+			printf("Value found between indexes [%ld] and [%ld]\n", k, i * m);
+			for (; k <= i * m; k++)
+			{
+				printf("Value checked array[%ld] = [%d]\n", k, array[k]);
+				if (array[k] == value)
+					return (k);
+			}
+		}
+
+		if (array[i * m] == value)
+			return (i * m);
+
+		i++;
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,7 +10,7 @@ int binary_search(int *array, size_t size, int value);
 int binary_search_util(int *array, int low, int high, int value);
 void print_array(int *array, int low, int high);
 
-
+int jump_search(int *array, size_t size, int value);
 
 
 
